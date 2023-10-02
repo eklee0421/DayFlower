f12e589c5a41f420f2689f57038c9dd95f472709
l2k <ekyi0421@gmail.com>
2023-10-02 17:46:01 +0900

    feat: okHttpClient ssl 우회 처리

--- app/src/main/java/com/nyang/dayFlower/data/service/SearchFlowerManager.kt
+++ app/src/main/java/com/nyang/dayFlower/data/service/SearchFlowerManager.kt
@@ -1,5 +1,6 @@
 package com.nyang.dayFlower.data.service
 
+import android.annotation.SuppressLint
 import com.google.gson.GsonBuilder
 import com.tickaroo.tikxml.TikXml
 import com.tickaroo.tikxml.retrofit.TikXmlConverterFactory
@@ -7,7 +8,12 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
 import java.util.concurrent.TimeUnit
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 
 object SearchFlowerManager {
     
@@ -22,11 +28,44 @@ object SearchFlowerManager {
         .addConverterFactory(GsonConverterFactory.create(GsonBuilder().setLenient().create()))
         .build()
 
-    private var okHttpClient = OkHttpClient.Builder()
+    private var okHttpClient = getUnsafeOkHttpClient()
         .connectTimeout(10, TimeUnit.MINUTES)
         .readTimeout(50, TimeUnit.SECONDS)
         .writeTimeout(50, TimeUnit.SECONDS)
         .build()
 
     fun getService(): SearchFlowerService = getInstance().create(SearchFlowerService::class.java)
+}
+
+/**
+ * Retrofit SSL 우회 접속 통신
+ */
+fun getUnsafeOkHttpClient(): OkHttpClient.Builder {
+    val trustAllCerts = arrayOf<TrustManager>(@SuppressLint("CustomX509TrustManager")
+    object : X509TrustManager {
+        @SuppressLint("TrustAllX509TrustManager")
+        override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?) {
+
+        }
+
+        @SuppressLint("TrustAllX509TrustManager")
+        override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) {
+
+        }
+
+        override fun getAcceptedIssuers(): Array<X509Certificate> {
+            return arrayOf()
+        }
+    })
+
+    val sslContext = SSLContext.getInstance("SSL")
+    sslContext.init(null, trustAllCerts, SecureRandom())
+
+    val sslSocketFactory = sslContext.socketFactory
+
+    val builder = OkHttpClient.Builder()
+    builder.sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager)
+    builder.hostnameVerifier { hostname, session -> true }
+
+    return builder
 }
