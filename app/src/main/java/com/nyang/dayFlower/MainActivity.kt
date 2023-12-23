1cae99bcd4b8d960e220c6bf0df31024238beb17
l2k <ekyi0421@gmail.com>
2023-12-23 23:03:11 +0900

    ui: status bar transparent 적용

--- app/src/main/java/com/nyang/dayFlower/MainActivity.kt
+++ app/src/main/java/com/nyang/dayFlower/MainActivity.kt
@@ -3,10 +3,15 @@ package com.nyang.dayFlower
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeContent
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
 import androidx.navigation.compose.rememberNavController
 import com.nyang.dayFlower.presentation.navigation.NavGraph
 import com.nyang.dayFlower.ui.theme.DayFlowerTheme
@@ -16,17 +21,20 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContent {
             DayFlowerTheme {
-                val navHostController = rememberNavController()
 
-                Surface(
+                    val navHostController = rememberNavController()
+                    Surface(
                         modifier = Modifier
                             .fillMaxSize(),
                         color = MaterialTheme.colorScheme.background
                     ) {
-                    NavGraph(navController = navHostController)
-                }
+                        NavGraph(navController = navHostController)
+                    }
 
             }
         }
