c891cb323c3d925bcc4048fbb8edc1b2be66303c
l2k <ekyi0421@gmail.com>
2024-01-04 14:11:08 +0900

    remove: 기존 flower detail 화면 삭제

--- app/src/main/java/com/nyangzzi/dayFlower/presentation/feature/mainFlower/MainFlowerScreen.kt
+++ app/src/main/java/com/nyangzzi/dayFlower/presentation/feature/mainFlower/MainFlowerScreen.kt
@@ -1,26 +1,19 @@
 package com.nyangzzi.dayFlower.presentation.feature.mainFlower
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -31,14 +24,10 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.nyangzzi.dayFlower.R
-import com.nyangzzi.dayFlower.domain.model.common.FlowerDetail
-import com.nyangzzi.dayFlower.presentation.feature.mainFlower.flowerDetail.FlowerDetailView
 import com.nyangzzi.dayFlower.presentation.navigation.HomeNavGraph
 import com.nyangzzi.dayFlower.presentation.navigation.Screens
 import com.nyangzzi.dayFlower.presentation.navigation.navigateSingleTopTo
 import com.nyangzzi.dayFlower.ui.theme.Gray5
-import java.time.LocalDate
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -69,17 +58,6 @@ fun MainFlowerScreen(
 
         FlowerBaseContent(navController = navController)
     }
-
-
-    /*FlowerBaseContent(
-        flowerDetail = uiState.flowerDetail,
-        flowerMonth= uiState.flowerMonth,
-        localDate = uiState.localDate,
-        isDatePicker = uiState.isDatePicker,
-        isCalendar = uiState.isCalendar,
-        onEvent = viewModel::onEvent
-        )*/
-
 }
 
 @Composable
@@ -121,65 +99,6 @@ private fun FlowerBaseContent(navController: NavHostController) {
                     navController.navigateSingleTopTo(item.screen.route)
                 }
             )
-        }
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-private fun FlowerBaseContents(
-    flowerDetail: FlowerDetail = FlowerDetail(),
-    flowerMonth: List<FlowerDetail> = emptyList(),
-    localDate: LocalDate = LocalDate.now(),
-    isDatePicker: Boolean = false,
-    isCalendar: Boolean = false,
-    onEvent: (MainFlowerEvent) -> Unit = {}
-) {
-
-    Scaffold(topBar = {
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 4.dp)
-                .height(56.dp)
-        ) {
-            Text("하루, 꽃", modifier = Modifier.align(Alignment.Center))
-
-            IconButton(
-                onClick = { onEvent(MainFlowerEvent.IsSearchDialog(true)) },
-                modifier = Modifier.align(Alignment.CenterStart)
-            ) {
-                Icon(Icons.Rounded.Search, "")
-            }
-
-            if (!isCalendar) {
-                IconButton(onClick = {
-                    onEvent(MainFlowerEvent.IsChangeView(isCalendar = true))
-                }, modifier = Modifier.align(Alignment.CenterEnd)) {
-                    Image(
-                        painter = painterResource(id = R.drawable.ic_calendar),
-                        contentDescription = null
-                    )
-                }
-            }
-
-        }
-
-    }) {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(it)
-        ) {
-
-
-            FlowerDetailView(
-                flowerDetail = flowerDetail,
-                localDate = localDate,
-                isDatePicker = isDatePicker,
-                onEvent = onEvent
-            )
-
         }
     }
 }
