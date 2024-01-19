61e06eaa1a5269f5fd1927ffcd6284bc495c1cee
l2k <ekyi0421@gmail.com>
2024-01-20 02:01:01 +0900

    ui: bottom bar calendar icon 변경

--- app/src/main/java/com/nyangzzi/dayFlower/presentation/feature/mainFlower/MainBottomNavItem.kt
+++ app/src/main/java/com/nyangzzi/dayFlower/presentation/feature/mainFlower/MainBottomNavItem.kt
@@ -5,7 +5,7 @@ import com.nyangzzi.dayFlower.presentation.navigation.Screens
 
 enum class MainBottomNavItem(val title: Int, val icon: Int, val screen: Screens) {
     Home(title = R.string.home, icon = R.drawable.ic_bottom_home, screen = Screens.Home),
-    Calendar(title = R.string.calendar, icon = R.drawable.ic_calendar, screen = Screens.Calendar),
+    Calendar(title = R.string.calendar, icon = R.drawable.ic_bottom_calendar, screen = Screens.Calendar),
     Search(title = R.string.search, icon = R.drawable.ic_bottom_search, screen = Screens.Search),
     Locker(title = R.string.locker, icon = R.drawable.ic_bottom_locker, screen = Screens.Locker),
     Profile(title = R.string.profile, icon = R.drawable.ic_bottom_profile, screen = Screens.Profile)
