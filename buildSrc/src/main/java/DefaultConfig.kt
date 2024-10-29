48162e94e18725bb1c879668cafd07db6806e5f5
l2k <ekyi0421@gmail.com>
2024-10-29 16:11:44 +0900

    build: target sdk 34 업데이트

--- buildSrc/src/main/java/DefaultConfig.kt
+++ buildSrc/src/main/java/DefaultConfig.kt
@@ -1,9 +1,9 @@
 object DefaultConfig {
-    const val COMPLIE_SDK = 33
+    const val COMPLIE_SDK = 34
     const val MIN_SDK = 28
-    const val TARGET_SDK = 33
-    const val VERSION_CODE = 1
-    const val VERSION_NAME = "1.0.0"
+    const val TARGET_SDK = 34
+    const val VERSION_CODE = 2
+    const val VERSION_NAME = "1.0.1"
 
     const val API_KEY_TYPE = "String"
     const val SEARCH_FLOWER = "search_flower_api_key"
